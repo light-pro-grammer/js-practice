1270c098c683391a0dc561cbb7c1ac38639d01e8
Ivan Parkhomenko <parkhomenko027@gmail.com>
2023-02-23 20:35:29 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const num1 = parseFloat(prompt('Enter first number:'));
+const num2 = parseFloat(prompt('Enter second number:'));
+
+const sum = num1 + num2;
+const diff = num1 - num2;
+const prod = num1 * num2;
+const quotient = num1 / num2;
+
+// Display the result in an alert window
+alert('Sum: ' + sum + '\nDifference: ' + diff + '\nProduct: ' + prod + '\nQuotient: ' + quotient);
