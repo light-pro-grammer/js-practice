3d0dd3e2ebc36045677873f448dae9c674b55ecc
Ivan Parkhomenko <parkhomenko027@gmail.com>
2023-02-24 12:03:10 +0200

    add conditions

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const num1 = parseFloat(prompt('Enter first number:'));
 const num2 = parseFloat(prompt('Enter second number:'));
 
-const sum = num1 + num2;
-const diff = num1 - num2;
-const prod = num1 * num2;
-const quotient = num1 / num2;
+if (isNaN(num1) || isNaN(num2)) {
+  alert('Incorrect data entered!');
+} else {
+  const sum = num1 + num2;
+  const diff = num1 - num2;
+  const prod = num1 * num2;
 
-// Display the result in an alert window
-alert('Sum: ' + sum + '\nDifference: ' + diff + '\nProduct: ' + prod + '\nQuotient: ' + quotient);
+  if (num2 === 0) {
+    alert('The second number cannot be zero!');
+  } else {
+    let quotient = num1 / num2;
+
+    if (num1 < num2) {
+      let confirmation = confirm('Are you sure you want to proceed with this operation?');
+      if (confirmation) {
+        alert(`Sum: ${sum}\nDifference: ${diff}\nProduct: ${prod}\nQuotient: ${quotient}`);
+      }
+    } else {
+      // Display the results in an alert window
+      alert(`Sum: ${sum}\nDifference: ${diff}\nProduct: ${prod}\nQuotient: ${quotient}`);
+    }
+  }
+}
